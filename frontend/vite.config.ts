812f978ac7d1706747f247ea1aebf2b678b6c7be
xadla <hadinjr4122@gmail.com>
2026-07-11 16:59:47 +0330

    feature(backend): add test configurations

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,20 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import { resolve } from 'node:path';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
+
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/test/setup.ts',
+  },
 });
