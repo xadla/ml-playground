df9ed1c56d121168edda1538815e6d54069104f1
xadla <hadinjr4122@gmail.com>
2026-07-11 16:50:35 +0330

    feature(frontend): add tailwind

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,11 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <h1>ML-Playground Frontend Test message</h1>
+    <div className="flex h-screen items-center justify-center">
+      <h1 className="text-5xl font-bold text-blue-500">Tailwind v4 works!</h1>
+    </div>
   </StrictMode>
 );
