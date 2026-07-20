d76f52c3b32c27c2c917858188003ee625a9e57c
xadla <hadinjr4122@gmail.com>
2026-07-20 08:39:45 +0330

    refactor(frontend): move to auth features

--- frontend/src/app/main.tsx
+++ frontend/src/app/main.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { BrowserRouter } from 'react-router-dom';
-import App from '@/App';
+import App from '@/app/App';
 import '@/index.css';
 
 const queryClient = new QueryClient();
